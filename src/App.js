4d869a572fb4ed39a4a6efc46b62f16f1ad56921
joao911 <joaopaulolacerda911@gmail.com>
2020-09-23 21:15:26 -0300

    componente de listas de filmes feito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import MovieRow from './components/Movierow';
+import Api from'./Api';
+import './App.css'
 
-import './App.css';
 
 function App() {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    const laodAll = async () =>{
+      // Pegando a lista total
+      let list = await Api.getHomeList()
+      setMovieList(list);
+    }
+    laodAll()
+  },[])
   return (
-    <div className="App">
-   <h1>inicio do projeto</h1>
-    </div>
+   <div className="page">
+     <section className="lists">
+       {movieList.map((item, key)=>(
+         <div>
+             <MovieRow key={key} title={item.title} items={item.items}/>
+         </div>
+       ))}
+     </section>
+   </div>
   );
 }
 
